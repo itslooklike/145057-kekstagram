9b9400e56fbfc796533d461aa9a5db1b0ef9e5ef
a.masalov <delsol@bk.ru>
2017-01-26 23:22:56 +0300

    add class remove to filter, optimize selector search

--- js/form.js
+++ js/form.js
@@ -21,41 +21,63 @@ window.onload = function () {
     uploadSelectImage.classList.remove('invisible');
   });
 
+  var inputWrap = document.querySelector('.upload-filter-controls');
+  var uploadFilterNone = inputWrap.querySelector('#upload-filter-none');
+  var uploadFilterChrome = inputWrap.querySelector('#upload-filter-chrome');
+  var uploadFilterSepia = inputWrap.querySelector('#upload-filter-sepia');
+  var uploadFilterMarvin = inputWrap.querySelector('#upload-filter-marvin');
+  var uploadFilterPhobos = inputWrap.querySelector('#upload-filter-phobos');
+  var uploadFilterHeat = inputWrap.querySelector('#upload-filter-heat');
+
+  var filterList = [
+    'filter-chrome',
+    'filter-sepia',
+    'filter-marvin',
+    'filter-phobos',
+    'filter-heat'
+  ];
+
   var filterImagePreview = document.querySelector('.filter-image-preview');
-  var uploadFilterChrome = document.querySelector('#upload-filter-chrome');
-  var uploadFilterSepia = document.querySelector('#upload-filter-sepia');
-  var uploadFilterMarvin = document.querySelector('#upload-filter-marvin');
-  var uploadFilterPhobos = document.querySelector('#upload-filter-phobos');
-  var uploadFilterHeat = document.querySelector('#upload-filter-heat');
+
+  function clearClassList() {
+    filterList.forEach(function classNameRemove(item) {
+      filterImagePreview.classList.remove(item);
+    });
+  }
+
+  uploadFilterNone.addEventListener('change', function filterNone() {
+    clearClassList();
+  });
 
   uploadFilterChrome.addEventListener('change', function filterChrome() {
-    // console.log(uploadFilterChrome.checked);
-    if (uploadFilterChrome.checked) {
-      filterImagePreview.classList.add('filter-chrome');
-    } else {
-      filterImagePreview.classList.remove('filter-chrome');
-    }
+    clearClassList();
+    filterImagePreview.classList.add('filter-chrome');
   });
 
   uploadFilterSepia.addEventListener('change', function filterSepia() {
+    clearClassList();
     filterImagePreview.classList.add('filter-sepia');
   });
 
   uploadFilterMarvin.addEventListener('change', function filterMarvin() {
+    clearClassList();
     filterImagePreview.classList.add('filter-marvin');
   });
 
   uploadFilterPhobos.addEventListener('change', function filterPhobos() {
+    clearClassList();
     filterImagePreview.classList.add('filter-phobos');
   });
 
   uploadFilterHeat.addEventListener('change', function filterHeat() {
+    clearClassList();
     filterImagePreview.classList.add('filter-heat');
   });
 
-  var uploadResizeControlsButtonInc = document.querySelector('.upload-resize-controls-button-inc');
-  var uploadResizeControlsButtonDec = document.querySelector('.upload-resize-controls-button-dec');
-  var uploadResizeControlsValue = document.querySelector('.upload-resize-controls-value');
+  var resizeControlWrap = document.querySelector('.upload-resize-controls');
+  var uploadResizeControlsButtonInc = resizeControlWrap.querySelector('.upload-resize-controls-button-inc');
+  var uploadResizeControlsButtonDec = resizeControlWrap.querySelector('.upload-resize-controls-button-dec');
+  var uploadResizeControlsValue = resizeControlWrap.querySelector('.upload-resize-controls-value');
 
   var currentZoom = 100;
   var maxZoom = 100;
