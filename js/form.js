47432bfc405e8c59e8d7dfd1498f2ec447cbf493
a.masalov <delsol@bk.ru>
2017-01-31 23:43:51 +0300

    aria and bubbling

--- js/form.js
+++ js/form.js
@@ -2,88 +2,100 @@
 
 window.onload = function () {
 
-  // Overlay toggle
-  var uploadOverlay = document.querySelector('.upload-overlay');
-  var uploadSelectImage = document.querySelector('#upload-select-image');
+  function overlayToggle() {
 
-  function toggleUploadForm(isOpening) {
-    if (isOpening) {
-      uploadOverlay.classList.remove('invisible');
-      uploadSelectImage.classList.add('invisible');
-    } else {
-      uploadOverlay.classList.add('invisible');
-      uploadSelectImage.classList.remove('invisible');
+    var uploadOverlay = document.querySelector('.upload-overlay');
+    var uploadSelectImage = document.querySelector('#upload-select-image');
+
+    function toggleUploadForm(isOpening) {
+      if (isOpening) {
+        uploadOverlay.classList.remove('invisible');
+        uploadSelectImage.classList.add('invisible');
+      } else {
+        uploadOverlay.classList.add('invisible');
+        uploadSelectImage.classList.remove('invisible');
+      }
     }
-  }
 
-  var uploadFile = document.querySelector('#upload-file');
-  uploadFile.addEventListener('change', function showUploadOverlay() {
-    toggleUploadForm(true);
-  });
+    var uploadFile = document.querySelector('#upload-file');
 
-  var uploadFormCancel = document.querySelector('.upload-form-cancel');
-  uploadFormCancel.addEventListener('click', function showUploadSelectImage() {
-    uploadSelectImage.reset();
+    uploadFile.addEventListener('change', function showUploadOverlay() {
+      toggleUploadForm(true);
+    });
+
+    var uploadFormCancel = document.querySelector('.upload-form-cancel');
+
+    uploadFormCancel.addEventListener('click', function showUploadSelectImage() {
+      uploadSelectImage.reset();
+      toggleUploadForm();
+    });
+
     toggleUploadForm();
-  });
+  }
 
-  toggleUploadForm();
+  function filtersChanger() {
 
-  // Filters
-  var inputWrap = document.querySelector('.upload-filter-controls');
-  var filterImagePreview = document.querySelector('.filter-image-preview');
+    var FILTER_LIST = [
+      'filter-none',
+      'filter-chrome',
+      'filter-sepia',
+      'filter-marvin',
+      'filter-phobos',
+      'filter-heat'
+    ];
 
-  var filterList = [
-    'filter-none',
-    'filter-chrome',
-    'filter-sepia',
-    'filter-marvin',
-    'filter-phobos',
-    'filter-heat'
-  ];
+    var filterImagePreview = document.querySelector('.filter-image-preview');
 
-  function clearClassList() {
-    filterList.forEach(function (item) {
-      filterImagePreview.classList.remove(item);
+    function clearClassList() {
+      FILTER_LIST.forEach(function (item) {
+        filterImagePreview.classList.remove(item);
+      });
+    }
+
+    var inputWrap = document.querySelector('.upload-filter-controls');
+
+    inputWrap.addEventListener('click', function (evt) {
+      if (evt.target.name === 'upload-filter') {
+        clearClassList();
+        filterImagePreview.classList.add('filter-' + evt.target.value);
+      }
     });
   }
 
-  filterList.forEach(function (item) {
-    var filterBtn = inputWrap.querySelector('#upload-' + item);
+  function zoomer() {
 
-    filterBtn.addEventListener('change', function filter() {
-      clearClassList();
-      filterImagePreview.classList.add(item);
+    var filterImagePreview = document.querySelector('.filter-image-preview');
+    var resizeControlWrap = document.querySelector('.upload-resize-controls');
+    var uploadResizeControlsButtonInc = resizeControlWrap.querySelector('.upload-resize-controls-button-inc');
+    var uploadResizeControlsButtonDec = resizeControlWrap.querySelector('.upload-resize-controls-button-dec');
+    var uploadResizeControlsValue = resizeControlWrap.querySelector('.upload-resize-controls-value');
+
+    var currentZoom = 100;
+    var maxZoom = 100;
+    var step = 25;
+
+    function setScale(value) {
+      filterImagePreview.style.transform = 'scale(' + parseInt(value, 10) / 100 + ')';
+    }
+
+    uploadResizeControlsValue.value = currentZoom + '%';
+
+    uploadResizeControlsButtonInc.addEventListener('click', function increaseZoom() {
+      var currentValue = parseInt(uploadResizeControlsValue.value, 10);
+
+      uploadResizeControlsValue.value = (currentValue + step > maxZoom ? maxZoom : currentValue + step) + '%';
+      setScale(uploadResizeControlsValue.value);
     });
-  });
 
-  // Zoom
-  var resizeControlWrap = document.querySelector('.upload-resize-controls');
-  var uploadResizeControlsButtonInc = resizeControlWrap.querySelector('.upload-resize-controls-button-inc');
-  var uploadResizeControlsButtonDec = resizeControlWrap.querySelector('.upload-resize-controls-button-dec');
-  var uploadResizeControlsValue = resizeControlWrap.querySelector('.upload-resize-controls-value');
+    uploadResizeControlsButtonDec.addEventListener('click', function decreaseZoom() {
+      var currentValue = parseInt(uploadResizeControlsValue.value, 10);
 
-  var currentZoom = 100;
-  var maxZoom = 100;
-  var step = 25;
-
-  function setScale(value) {
-    filterImagePreview.style.transform = 'scale(' + parseInt(value, 10) / 100 + ')';
+      uploadResizeControlsValue.value = (currentValue - step < step ? step : currentValue - step) + '%';
+      setScale(uploadResizeControlsValue.value);
+    });
   }
 
-  uploadResizeControlsValue.value = currentZoom + '%';
-
-  uploadResizeControlsButtonInc.addEventListener('click', function increaseZoom() {
-    var currentValue = parseInt(uploadResizeControlsValue.value, 10);
-
-    uploadResizeControlsValue.value = (currentValue + step > maxZoom ? maxZoom : currentValue + step) + '%';
-    setScale(uploadResizeControlsValue.value);
-  });
-
-  uploadResizeControlsButtonDec.addEventListener('click', function decreaseZoom() {
-    var currentValue = parseInt(uploadResizeControlsValue.value, 10);
-
-    uploadResizeControlsValue.value = (currentValue - step < step ? step : currentValue - step) + '%';
-    setScale(uploadResizeControlsValue.value);
-  });
+  overlayToggle();
+  filtersChanger();
+  zoomer();
 };
