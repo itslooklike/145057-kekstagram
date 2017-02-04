879319c12faae0865dc41886fba8bc1b82e5ac67
a.masalov <delsol@bk.ru>
2017-02-04 19:31:16 +0300

    some refact, use 'bind'

--- js/form.js
+++ js/form.js
@@ -17,8 +17,6 @@ window.onload = function () {
       'filter-heat'
     ];
 
-    var EDITOR_DEFAULT_ZOOM = 100;
-
     var picture = document.querySelector('.filter-image-preview');
 
     function setPictureScale(value) {
@@ -35,18 +33,11 @@ window.onload = function () {
       if (evt.target.name === 'upload-filter') {
         clearPictureFilters();
         picture.classList.add('filter-' + evt.target.value);
-      } else if (evt.keyCode && evt.keyCode === KEY_CODE.enter) {
+      } else if (evt.keyCode === KEY_CODE.enter) {
         clearPictureFilters();
         evt.target.control.checked = true;
         picture.classList.add('filter-' + evt.target.control.value);
       }
-    }
-
-    var zoomValue = document.querySelector('.upload-resize-controls-value');
-
-    function editorZoomReset() {
-      zoomValue.value = EDITOR_DEFAULT_ZOOM + '%';
-      setPictureScale(EDITOR_DEFAULT_ZOOM);
     }
 
     var editorWindow = document.querySelector('.upload-overlay');
@@ -61,11 +52,12 @@ window.onload = function () {
     var filtersWrap = document.querySelector('.upload-filter-controls');
 
     function closePhtotoEditor(evt) {
-      if (evt.keyCode && evt.keyCode === KEY_CODE.escape || evt.type === 'click') {
+      if (evt.keyCode === KEY_CODE.escape || evt.type === 'click') {
         uploadImageForm.reset();
         clearPictureFilters();
         toggleUploadForm();
         filtersWrap.removeEventListener('click', filterChanger);
+        filtersWrap.removeEventListener('keydown', filterChanger);
       }
     }
 
@@ -79,6 +71,15 @@ window.onload = function () {
       filtersHandlersOn();
     }
 
+    var zoomValue = document.querySelector('.upload-resize-controls-value');
+
+    function editorZoomReset() {
+      var editorDefaultZoom = 100;
+
+      zoomValue.value = editorDefaultZoom + '%';
+      setPictureScale(editorDefaultZoom);
+    }
+
     function zoomer() {
       var maxZoom = 100;
       var step = 25;
@@ -88,21 +89,19 @@ window.onload = function () {
       var dec = controlWrap.querySelector('.upload-resize-controls-button-dec');
 
       function changeZoom(increase) {
-        return function () {
-          var currentValue = parseInt(zoomValue.value, 10);
+        var currentValue = parseInt(zoomValue.value, 10);
 
-          if (increase) {
-            zoomValue.value = (currentValue + step > maxZoom ? maxZoom : currentValue + step) + '%';
-          } else {
-            zoomValue.value = (currentValue - step < step ? step : currentValue - step) + '%';
-          }
+        if (increase) {
+          zoomValue.value = (currentValue + step > maxZoom ? maxZoom : currentValue + step) + '%';
+        } else {
+          zoomValue.value = (currentValue - step < step ? step : currentValue - step) + '%';
+        }
 
-          setPictureScale(zoomValue.value);
-        };
+        setPictureScale(zoomValue.value);
       }
 
-      var increaseZoom = changeZoom(true);
-      var decreaseZoom = changeZoom(false);
+      var increaseZoom = changeZoom.bind(changeZoom, true);
+      var decreaseZoom = changeZoom.bind(changeZoom, false);
 
       inc.addEventListener('click', increaseZoom);
       dec.addEventListener('click', decreaseZoom);
